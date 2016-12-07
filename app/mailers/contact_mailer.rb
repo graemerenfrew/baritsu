06ba8b1032e786ee9402a3f9d224abc6386391b8
Graeme Renfrew <graemerenfrew+github@gmail.com>
2016-12-07 17:34:31 +0000

    added contact mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer
+  default to: 'graemerenfrew+contactusbaritsudev@gmail.com'
+  
+  def contact_email(name, email, body)
+      @name = name
+      @email = email
+      @body = body
+      
+      mail(from: email, subject: 'Contact Form Message')
+  end
+end
