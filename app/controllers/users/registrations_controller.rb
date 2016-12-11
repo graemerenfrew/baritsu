07612080e54e2119e812eafb005f7a03f446935b
Graeme Renfrew <graemerenfrew+github@gmail.com>
2016-12-11 13:52:58 +0000

    added redirect if they try to spoof plan

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
+  before_action :select_plan, only: :new
+
   #extend the default devise gem behaviour so that users signing up with
   #pro account (plan_id 2) get a subscription created on stripe
   #Otherwise devise signs up with standard sign-up process
@@ -15,4 +16,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+  
+  private 
+    def select_plan
+      unless (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
+      end
+    end
 end
