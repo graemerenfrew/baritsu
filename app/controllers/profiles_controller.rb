7feb713e3c46f1c1593aaee066975e900e2249df
Graeme Renfrew <graemerenfrew+github@gmail.com>
2016-12-12 21:39:17 +0000

    added before authentication filters to profile action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,6 +21,28 @@ class ProfilesController < ApplicationController
       end
     end
     
+    # GET to /users/:user_id/profile/edit
+    def edit
+      @user = User.find ( params[:user_id])
+      @profile = @user.profile
+    end
+    
+    #PUT  /users/:user_id/profile
+    def update
+      #retrieve user from the db
+      @user = User.find ( params[:user_id])
+      @profile = @user.profile
+      #mass assign edited profile attributes and save them
+      if @profile.update_attributes(profile_params)
+        flash[:success] = "Profile updated."
+        
+        #redirect user to their profile page
+        redirect_to user_path(id: params[:user_id])
+      else
+        render action: :edit
+      end
+    end 
+    
     private
       def profile_params
         params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
