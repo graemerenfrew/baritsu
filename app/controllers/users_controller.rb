96271e82cc0802b9a9dfea7699712c23d09e5a27
Graeme Renfrew <graemerenfrew+github@gmail.com>
2016-12-13 06:29:57 +0000

    housekeeping commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   
   # GET to /users/:id
   def show
     @user = User.find ( params[:id])
   end
+  
+  #GET to /users
+  def index
+    @users = User.includes(:profile)
+  end  
+    
 end
